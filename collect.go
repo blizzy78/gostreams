a3ebc8d1bf96310d98bd5a85015f9a4298a63651
Maik Schreiber <blizzy@blizzy.de>
2023-09-30 16:32:19 +0200

    refactoring

--- collect.go
+++ collect.go
@@ -25,30 +25,31 @@ func CollectSlice[T any]() CollectorFunc[T, []T] {
 // CollectMap returns a collector that collects elements into a map, using key to map elements to keys,
 // and value to map elements to values. If a key is already in the map, it overwrites the map entry.
 func CollectMap[T any, K comparable, V any](key MapperFunc[T, K], value MapperFunc[T, V]) CollectorFunc[T, map[K]V] {
-	result := map[K]V{}
-
-	return func(ctx context.Context, cancel context.CancelCauseFunc, elem T, index uint64) map[K]V {
-		result[key(ctx, cancel, elem, index)] = value(ctx, cancel, elem, index)
-		return result
-	}
+	return collectMap[T, K, V](key, value, true)
 }
 
 // CollectMapNoDuplicateKeys returns a collector that collects elements into a map, using key to map
 // elements to keys, and value to map elements to values. If a key is already in the map, it cancels the stream's context
 // with a DuplicateKeyError.
 func CollectMapNoDuplicateKeys[T any, K comparable, V any](key MapperFunc[T, K], value MapperFunc[T, V]) CollectorFunc[T, map[K]V] {
+	return collectMap[T, K, V](key, value, false)
+}
+
+func collectMap[T any, K comparable, V any](key MapperFunc[T, K], value MapperFunc[T, V], overwriteEntries bool) CollectorFunc[T, map[K]V] {
 	result := map[K]V{}
 
 	return func(ctx context.Context, cancel context.CancelCauseFunc, elem T, index uint64) map[K]V {
 		key := key(ctx, cancel, elem, index)
 
-		if _, ok := result[key]; ok {
-			cancel(&DuplicateKeyError[T, K]{
-				Element: elem,
-				Key:     key,
-			})
+		if !overwriteEntries {
+			if _, ok := result[key]; ok {
+				cancel(&DuplicateKeyError[T, K]{
+					Element: elem,
+					Key:     key,
+				})
 
-			return result
+				return result
+			}
 		}
 
 		result[key] = value(ctx, cancel, elem, index)
