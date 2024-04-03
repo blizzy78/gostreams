18f2cdd00f2d8675bcea19349191886e2142caf8
Maik Schreiber <blizzy@blizzy.de>
2024-04-03 03:31:37 +0200

    move example to gostreams_test package

--- example_test.go
+++ example_test.go
@@ -1,33 +1,36 @@
-package gostreams
+package gostreams_test
 
 import (
 	"context"
 	"fmt"
 	"strconv"
+
+	"github.com/blizzy78/gostreams"
 )
 
 func Example() {
 	// construct a producer from a slice
-	ints := Produce([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
+	ints := gostreams.Produce([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
 
 	// filter for even elements
 	// since we only need the elements themselves, we can use FuncPredicate
-	ints = Filter(ints, FuncPredicate(func(elem int) bool {
+	ints = gostreams.Filter(ints, gostreams.FuncPredicate(func(elem int) bool {
 		return elem%2 == 0
 	}))
 
 	// map elements by doubling them
 	// since we only need the elements themselves, we can use FuncMapper
-	ints = Map(ints, FuncMapper(func(elem int) int {
+	ints = gostreams.Map(ints, gostreams.FuncMapper(func(elem int) int {
 		return elem * 2
 	}))
 
 	// map elements by converting them to strings
-	intStrs := Map(ints, FuncMapper(strconv.Itoa))
+	intStrs := gostreams.Map(ints, gostreams.FuncMapper(strconv.Itoa))
 
 	// perform a reduction to collect the strings into a slice
-	strs, _ := ReduceSlice(context.Background(), intStrs)
+	strs, _ := gostreams.ReduceSlice(context.Background(), intStrs)
 
 	fmt.Printf("%+v\n", strs)
+
 	// Output: [4 8 12 16 20]
 }
